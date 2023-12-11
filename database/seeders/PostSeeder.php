83b8b45bd39e443de44e2f878802183b9065a508
kiipod <iamkir90@hotmail.com>
2023-12-11 20:49:55 +0300

    Добавляет миграции, фабрики и сиды для БД

---
+++ database/seeders/PostSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Sequence;
+use Illuminate\Database\Seeder;
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Post::factory()->count(50)->state(new Sequence(
+            fn ($sequence) => ['user_id' => User::all()->random()]
+        ))->create();
+    }
+}
