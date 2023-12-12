fe997b6df5e2b9347e04e1d389ed52835916b56e
kiipod <iamkir90@hotmail.com>
2023-12-12 23:09:58 +0300

    Добавляет роуты для AuthController

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('auth')->middleware('api')->group(function () {
+    Route::post('/login', [AuthController::class, 'login'])->name('auth.login');
+    Route::post('/register', [AuthController::class, 'register'])->name('auth.register');
+    Route::post('/logout', [AuthController::class, 'logout'])->name('auth.logout');
+    Route::post('/refresh', [AuthController::class, 'refresh'])->name('auth.refresh');
 });
