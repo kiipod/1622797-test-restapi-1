faee7060e0f4238bb63577f483d6a87baa62e8cd
kiipod <iamkir90@hotmail.com>
2023-12-13 18:04:30 +0300

    Добавляет модель, request, controller для работы с новостями

---
+++ app/Models/Post.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    /**
+    * The table associated with the model.
+    *
+    * @var string
+    */
+    protected $table = 'posts';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    public $fillable = [
+        'title',
+        'fragment',
+        'content'
+    ];
+
+    /**
+     * Получение пользователя создавшего новость
+     *
+     * @return BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
